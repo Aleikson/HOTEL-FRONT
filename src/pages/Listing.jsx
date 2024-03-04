72fcbfb1fe467255436976c6915a7191c8523a98
Aleikson <aleiksonsilva@hotmail.com>
2024-03-04 18:07:09 -0300

    Feat contact user

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -12,6 +12,8 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import Contact from '../components/Contact';
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -21,7 +23,9 @@ export default function Listing() {
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
 
+  const [contact, setContact] = useState(false);
   const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -131,6 +135,19 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {listing &&
+              listing.userRef &&
+              currentUser &&
+              listing.userRef._id !== currentUser._id &&
+              !contact && (
+                <button
+                  onClick={() => setContact(true)}
+                  className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+                >
+                  Contact landlord
+                </button>
+              )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
